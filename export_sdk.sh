23d9c62c43514183e8a08e660ecb9a94a4206385
root <root@DESKTOP-7G9Q77B.localdomain>
2022-06-15 17:08:00 +0200

    removed build and updated gitignore

--- export_sdk.sh
+++ export_sdk.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-export PICO_SDK_PATH=/home/pico/pico-sdk/
-export PICO_EXAMPLES_PATH=/home/pico/pico-examples
+export PICO_SDK_PATH=/home/RpiPico/pico-sdk/
+export PICO_EXAMPLES_PATH=/home/RpiPico/pico-examples
